d0241471dc78308e9688e3c753c6cb59d105fe79
Jeremy Sawruk <jeremy.sawruk@gmail.com>
2011-11-05 17:03:01 -0400

    Initial layout

--- index.php
+++ index.php
@@ -3,9 +3,32 @@
 	<head>
 		<title>FMA Radio</title>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="css/main.css" />
 	</head>
 	<body>
-	<?php print "FMA"; ?>
+	
+	<div class="topbar">
+      <div class="topbar-inner">
+        <div class="container-fluid">
+          <a class="brand" href="#">FMA Radio</a>
+        </div><!-- /.container-fluid -->
+      </div><!-- /.topbar-inner -->
+    </div><!-- /.topbar -->
+	
+	<div class="container-fluid">
+		<div class="sidebar">
+			<div class="well">
+			SIDEBAR
+			</div><!-- ./well -->
+		</div><!-- /.sidebar -->
+
+		<div class="content">
+			<div class="hero-unit">
+			CONTENT
+			</div><!-- ./hero-unit -->
+		</div><!-- /.content -->
+	</div><!-- /.container-fluid -->
+	
 	<script type="text/javascript" src="js/jquery-1.7.min.js"></script>
 	</body>
 </html>
