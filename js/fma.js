300a9a36bf78261697756bfba9edd2a28ac62f8c
Jeremy Sawruk <jeremy.sawruk@gmail.com>
2011-11-05 17:30:31 -0400

    Echonest test

--- js/fma.js
+++ js/fma.js
@@ -1,4 +1,22 @@
 // FMA Radio main Javascript file
 var fma = {};
+fma.apiKey = "RVT1BNUNP3DUTKTLS";
+
 fma.load = function() {
+	fma.getPlaylist();
 };
+
+fma.getPlaylist = function() {
+	$.ajax({
+		url: "http://developer.echonest.com/api/v4/playlist/dynamic",
+		data:{
+			api_key: 'N6E4NIOVYMTHNDM8J',
+			artist:	'Yacht',
+			type: 'artist-radio',
+			bucket: 'id:fma'
+		},
+		success: function(data) {
+			console.log(data);
+		}
+	});
+}
