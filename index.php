21e51188abf15cb90e0fc2be4340604ca7349d1e
Conductive IO <admin@conductiveio.com>
2011-11-05 16:50:01 -0400

    added jqeury

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html>
 	<head>
 		<title>FMA Radio</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
 	</head>
 	<body>
 	<?php print "FMA"; ?>
+	<script type="text/javascript" src="js/jquery-1.7.min.js"></script>
 	</body>
 </html>
