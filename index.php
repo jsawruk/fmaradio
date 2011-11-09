65a1625b90df47c6523a37fe7e514e793c5faa96
Jeremy Sawruk <jeremy.sawruk@gmail.com>
2011-11-08 20:58:09 -0500

    Add Google Analytics

--- index.php
+++ index.php
@@ -8,6 +8,21 @@
 		<link rel="stylesheet" type="text/css" href="css/main.css" />
 		<script src="js/head.load.min.js"></script>
 		<!-- Load head.load.js in head, load others using this in body -->
+		
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-26895827-1']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		
+		</script>
+		
 	</head>
 	<body>
 		<div class="topbar">
